7423e93f40ac0ac08f7645b72b58aa46c5c49fd3
CesarCorderoAndrade <Curso de SQL desde Cero>
2026-04-20 14:35:09 +0200

    Update client

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -5,9 +5,12 @@
 
 import axios from 'axios';
 
+const apiURL = import.meta.env.PROD 
+  ? '/api' 
+  : 'http://localhost:3000/api';
+
 const apiClient = axios.create({
-  // Forzamos que todas las peticiones vayan al puerto 3000 donde vive Express
-  baseURL: 'http://localhost:3000/api',
+  baseURL: apiURL,
   headers: {
     'Content-Type': 'application/json'
   }
